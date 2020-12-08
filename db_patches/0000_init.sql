97d14841bc8cd70747abf86978556d05f9cbbea4
jekabskarklins <jekabs.karklins@ess.eu>
2020-12-08 10:04:42 +0100

    SWAP-1200 Adding another user to test data

--- db_patches/0000_init.sql
+++ db_patches/0000_init.sql
@@ -288,7 +288,7 @@ INSERT INTO users (
                       'Carlsson', 
                       'testuser', 
                       '$2a$10$1svMW3/FwE5G1BpE7/CPW.aMyEymEBeWK4tSTtABbsoo/KaSQ.vwm',
-                      '123123123',
+                      'Carl',
                       '123123123',
                       '581459604',
                       'male',
@@ -305,7 +305,6 @@ INSERT INTO users (
                       );
 
     INSERT INTO role_user (role_id, user_id) VALUES (1, 1);
-
 
 
 
@@ -404,6 +403,54 @@ INSERT INTO users (
                     );
 
     INSERT INTO role_user (role_id, user_id) VALUES (3, 3);
+
+    INSERT INTO users (
+                      user_title, 
+                      firstname, 
+                      middlename, 
+                      lastname, 
+                      username, 
+                      password,
+                      preferredname,
+                      orcid,
+                      orcid_refreshToken,
+                      gender,
+                      nationality,
+                      birthdate,
+                      organisation,
+                      department,
+                      organisation_address,
+                      position,
+                      email,
+                      email_verified,
+                      telephone,
+                      telephone_alt
+                      ) 
+    VALUES 
+                    (
+                      'Mr.', 
+                      'Benjamin',
+                      'Bryson', 
+                      'Beckley', 
+                      'testuser2', 
+                      '$2a$10$1svMW3/FwE5G1BpE7/CPW.aMyEymEBeWK4tSTtABbsoo/KaSQ.vwm',
+                      'Benjamin',
+                      '123123123',
+                      '581459604',
+                      'male',
+                      'Danish',
+                      '2000-04-02',
+                      'Roberts, Reilly and Gutkowski',
+                      'IT deparment',
+                      'Denmark, Carlton Road, 2100 Riverside Avenue',
+                      'Management',
+                      'ben@inbox.com',
+                      true,
+                      '(288) 221-4533',
+                      '(370) 555-4432'
+                      );
+
+    INSERT INTO role_user (role_id, user_id) VALUES (1, 4);
 
 
     INSERT INTO call(
