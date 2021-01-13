0087ab16b3172328bcd9b2bcea64976568ac7e21
jekabskarklins <jekabs.karklins@ess.eu>
2021-01-13 17:20:36 +0100

    SWAP-756 adding feature toggle #resolved

---
+++ db_patches/0076_CreateFeatureToggle.sql
@@ -0,0 +1,18 @@
+DO
+$$
+DECLARE
+    shipmentCategoryId int;
+BEGIN
+	IF register_patch('CreateFeatureToggle.sql', 'jekabskarklins', 'Create Feature Toggle', '2021-01-13') THEN
+	BEGIN
+
+      CREATE TABLE features (
+            feature_id VARCHAR(128) PRIMARY KEY
+          , is_enabled BOOLEAN DEFAULT false 
+          , description VARCHAR(500) NOT NULL DEFAULT ''
+      ); 
+    END;
+	END IF;
+END;
+$$
+LANGUAGE plpgsql;
