5abfc214a2a852841d2834f9889542ab346129b9
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2021-02-03 11:37:02 +0100

    db patch fixed

--- db_patches/0078_AddRichTextInputDataType.sql
+++ db_patches/0078_AddRichTextInputDataType.sql
@@ -3,7 +3,7 @@ $$
 BEGIN
     IF register_patch('AddRichTextInputDataType.sql', 'Peter Asztalos', 'Add Rich Text Input Data type', '2021-02-02') THEN
 
-      INSERT INTO "question_datatypes" ("question_datatype_id") VALUES ('RICH_TEXT_INPUT')
+      INSERT INTO "question_datatypes" ("question_datatype_id") VALUES ('RICH_TEXT_INPUT');
 
     END IF;
 END;
