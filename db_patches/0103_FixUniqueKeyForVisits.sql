b7b048f3cc343b3b1e7992d298caf03be45d362b
jekabskarklins <jekabs.karklins@ess.eu>
2021-08-02 08:45:18 +0300

    fix name

--- db_patches/0103_FixUniqueKeyForVisits.sql
+++ db_patches/0103_FixUniqueKeyForVisits.sql
@@ -1,7 +1,7 @@
 DO
 $$
 BEGIN
-	IF register_patch('AddCompositeUniqueKeyForVisits.sql', 'Jekabs Karklins', 'Add composite primary key for visits', '2021-08-02') THEN
+	IF register_patch('FixUniqueKeyForVisits.sql', 'Jekabs Karklins', 'Fix UNIQUE constraint for the visit table', '2021-08-02') THEN
 		BEGIN
 			DROP INDEX visits_proposal_pk;
 			ALTER TABLE visits add CONSTRAINT visits_proposal_pk_scheduled_event_id UNIQUE (proposal_pk, scheduled_event_id);
