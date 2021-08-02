aa23be4d268226f42c700127bf2c91527466ffd9
jekabskarklins <jekabs.karklins@ess.eu>
2021-08-02 08:47:29 +0300

    fix the unique

--- db_patches/0103_FixUniqueKeyForVisits.sql
+++ db_patches/0103_FixUniqueKeyForVisits.sql
@@ -4,7 +4,7 @@ BEGIN
 	IF register_patch('FixUniqueKeyForVisits.sql', 'Jekabs Karklins', 'Fix UNIQUE constraint for the visit table', '2021-08-02') THEN
 		BEGIN
 			DROP INDEX visits_proposal_pk;
-			ALTER TABLE visits add CONSTRAINT visits_proposal_pk_scheduled_event_id UNIQUE (proposal_pk, scheduled_event_id);
+			ALTER TABLE visits add CONSTRAINT visits_scheduled_event_id UNIQUE (scheduled_event_id);
 		END;
 	END IF;
 END;
