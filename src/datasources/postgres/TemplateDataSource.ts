c4ab883b66c81578454a2da03ad06757a378b1b2
jekabskarklins <jekabs.karklins@ess.eu>
2022-02-10 14:21:26 +0100

    fix(tests): fix failing tests

--- src/datasources/postgres/TemplateDataSource.ts
+++ src/datasources/postgres/TemplateDataSource.ts
@@ -50,8 +50,8 @@ import {
   TopicRecord,
 } from './records';
 
-const EXPORT_VERSION = '1.0.0';
-const MIN_SUPPORTED_VERSION = '1.0.0';
+const EXPORT_VERSION = '1.1.0';
+const MIN_SUPPORTED_VERSION = '1.1.0';
 
 export default class PostgresTemplateDataSource implements TemplateDataSource {
   async getTemplateCategories(): Promise<TemplateCategory[]> {
