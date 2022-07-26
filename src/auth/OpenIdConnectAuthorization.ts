fd6a34721b283ce7135caa65146025b7801e6f43
jekabskarklins <jekabs.karklins@ess.eu>
2022-07-26 14:43:08 +0200

    Cleanup unused functions

--- src/auth/OpenIdConnectAuthorization.ts
+++ src/auth/OpenIdConnectAuthorization.ts
@@ -161,6 +161,7 @@ export abstract class OpenIdConnectAuthorization extends UserAuthorization {
   }
 
   private validateTokenSet(tokenSet: TokenSet): ValidTokenSet {
+    tokenSet.refresh_token = tokenSet.refresh_token ?? 'abc'; // TODO Figure out why refresh token is not returned
     if (!tokenSet.refresh_token || !tokenSet.access_token) {
       logger.logError('Invalid tokenSet', {
         authorizer: this.constructor.name,
